328d6e244ca1dcfb457c97db7eb5e1a966767f3e
Mateus Augusto dos Santos Fonseca <mateus.asfonseca@gmail.com>
2023-01-13 19:50:08 -0300

    identação appmodule

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,12 +6,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 import { FooterComponent } from './components/footer/footer.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { HomeAdmComponent } from './admin/components/home-adm/home-adm.component';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { OptionsAdmComponent } from './admin/components/options-adm/options-adm.component';
 import { CreateEventComponent } from './admin/components/create-event/create-event.component';
 import { ListEventsComponent } from './admin/components/list-events/list-events.component';
