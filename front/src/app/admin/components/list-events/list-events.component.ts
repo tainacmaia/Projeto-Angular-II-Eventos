2483d2e2ee79259af8940b115970afac90a2b142
Mateus Augusto dos Santos Fonseca <mateus.asfonseca@gmail.com>
2023-01-13 18:31:28 -0300

    exclusão de método não usado

--- front/src/app/admin/components/list-events/list-events.component.ts
+++ front/src/app/admin/components/list-events/list-events.component.ts
@@ -24,7 +24,7 @@ export class ListEventsComponent {
     this.getEventsList();
   }
 
-  private getEventsList ():void {
+  public getEventsList ():void {
     this.events$ = this.eventsService.getEventsList()
     this.events$.subscribe({next: (event: EventModel[]) => {
       this.events = event} })
--- front/src/app/admin/service/adm-events.service.ts
+++ front/src/app/admin/service/adm-events.service.ts
@@ -6,14 +6,9 @@ import { EventModel } from "../models/event.model";
 @Injectable({
   providedIn: 'root'
 })
+
 export class EventsService {
   constructor(private http: HttpClient) { }
-
-
-  public getEvents(): Event[] {
-    const events = JSON.parse(localStorage.getItem('EVENTS') || '[]');
-    return events;
-  }
 
   public getEventsList(): Observable<EventModel[]> {
     return this.http.get<EventModel[]>('http://localhost:5000/events');
