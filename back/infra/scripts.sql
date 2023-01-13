5cc0ee1ca9f17063ce01fd28ae27832fdc7f94c6
Tainá Maia <taina.cmaia@hotmail.com>
2023-01-13 13:49:58 -0300

    correção eventos

--- back/infra/scripts.sql
+++ back/infra/scripts.sql
@@ -1,32 +1,16 @@
 CREATE TABLE events(
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    title VARCHAR(40) NOT NULL,
-    date TEXT NOT NULL
-)
-
-CREATE TABLE addresses(
-    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    zipCode INTEGER(8) NOT NULL,
-    street VARCHAR(100) NOT NULL,
-    number INTEGER(10) NOT NULL,
-    complement VARCHAR(100) NULL,
-    neighborhood VARCHAR(50) NOT NULL,
-    city VARCHAR(50) NOT NULL,
-    state VARCHAR(30) NOT NULL,
-    eventId INTEGER NOT NULL,
-    CONSTRAINT fk_eventId
-    FOREIGN KEY (eventId)
-    REFERENCES events (id)
+    title TEXT NOT NULL,
+    description TEXT NULL,
+    date TEXT NOT NULL,
+    local TEXT NOT NULL,
+    price INTEGER NOT NULL,
+    address TEXT NOT NULL
 )
 
 SELECT * FROM events;
-SELECT * FROM addresses;
 
 DROP TABLE events;
-DROP TABLE addresses;
 
-INSERT INTO events(title,date)
-VALUES ('Teste', datetime('now'))
-
-INSERT INTO addresses(zipCode, street, number, complement, neighborhood, city, state, eventId)
-VALUES (123456789, 'Rua tralala', 1, 'tralala', 'tralala', 'tralala', 'tralala', 1)
+INSERT INTO events(title,description,date, local, price, address)
+VALUES ('Teste', 'Testinho', datetime('now'), 'Estádio Unicórnio', 200, 'Rua do Unicórnio, 666')
