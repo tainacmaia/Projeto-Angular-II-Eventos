a5d9b2bbc182ec437a04993377401d7c5a589cb0
Mateus Augusto dos Santos Fonseca <mateus.asfonseca@gmail.com>
2023-01-16 18:31:17 -0300

    melhora de componente de listagem e reserva

---
+++ front/src/app/user/models/reservation.model.ts
@@ -0,0 +1,11 @@
+export interface Reservation {
+  id: string;
+  title: string;
+  description: string;
+  date: string;
+  local: string;
+  price: number;
+  address: string;
+  createdAt: null;
+  updateAt: null;
+}
