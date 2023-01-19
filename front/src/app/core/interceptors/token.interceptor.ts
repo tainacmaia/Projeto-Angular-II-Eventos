041c6d070145df62e34719af5c69d1ef00f3cbdf
Mateus Augusto dos Santos Fonseca <mateus.asfonseca@gmail.com>
2023-01-19 17:29:53 -0300

    correção do interceptor

--- front/src/app/core/interceptors/token.interceptor.ts
+++ front/src/app/core/interceptors/token.interceptor.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor} from '@angular/common/http';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
 import { LocalStorageKeysEnum } from '../constants/local-storage-keys.enum';
@@ -7,17 +7,18 @@ import { LocalStorageKeysEnum } from '../constants/local-storage-keys.enum';
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) { }
 
   intercept(
     request: HttpRequest<any>,
     next: HttpHandler
   ): Observable<HttpEvent<any>> {
-    if (request.url.includes('/login') || request.url.includes('/events')) {
+    if (request.url.includes('/login') || (request.url.includes('/events') && request.method === "GET")) {
       return next.handle(request);
     }
 
     const token = localStorage.getItem(LocalStorageKeysEnum.ADM_TOKEN);
+
 
     if (!token) {
       this.router.navigate(['/adm/login']);
